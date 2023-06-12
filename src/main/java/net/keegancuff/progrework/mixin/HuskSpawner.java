885aa42c0b0601d358586e50bbb3811ec2bd9151
KeeganCuff <keegancuff@gmail.com>
2023-06-11 19:47:06 -0500

    Fixed issue with sand dust translation Increased husk spawner generation and added them to badlands

--- src/main/java/net/keegancuff/progrework/mixin/HuskSpawner.java
+++ src/main/java/net/keegancuff/progrework/mixin/HuskSpawner.java
@@ -3,6 +3,8 @@ package net.keegancuff.progrework.mixin;
 import net.minecraft.block.entity.MobSpawnerBlockEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -19,15 +21,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.function.Predicate;
+
 @Mixin(DungeonFeature.class)
 public class HuskSpawner {
-    private static final EntityType<?>[] DESERT_MOB_SPAWNER_ENTITIES = new EntityType[]{EntityType.SKELETON, EntityType.HUSK, EntityType.ZOMBIE, EntityType.SPIDER};
+    private static final EntityType<?>[] DESERT_MOB_SPAWNER_ENTITIES = new EntityType[]{EntityType.SKELETON, EntityType.HUSK, EntityType.HUSK, EntityType.HUSK, EntityType.SPIDER};
     @Inject(method = "generate", at = @At("RETURN"))
     private void injected(FeatureContext<DefaultFeatureConfig> context, CallbackInfoReturnable ci){
         StructureWorldAccess world = context.getWorld();
         BlockPos pos = context.getOrigin();
         Random random = context.getRandom();
-        if (world.getBiome(pos).matchesKey(BiomeKeys.DESERT) && world.getBlockEntity(pos) instanceof MobSpawnerBlockEntity mobSpawnerBlockEntity){
+        RegistryEntry<Biome> biome = world.getBiome(pos);
+        if ((biome.matchesKey(BiomeKeys.DESERT) || biome.matchesKey(BiomeKeys.WOODED_BADLANDS) ||
+                biome.matchesKey(BiomeKeys.BADLANDS) || biome.matchesKey(BiomeKeys.ERODED_BADLANDS)) &&
+                world.getBlockEntity(pos) instanceof MobSpawnerBlockEntity mobSpawnerBlockEntity){
             mobSpawnerBlockEntity.setEntityType(getMobSpawnerEntity(random), random);
         }
     }
